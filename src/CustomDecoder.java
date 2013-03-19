b33983b964076f7fab8916646713f4c6023febee
Cody Canning <cocanning11@gmail.com>
2013-03-19 18:09:38 -0400

    added feature extraction

--- src/CustomDecoder.java
+++ src/CustomDecoder.java
@@ -5,7 +5,7 @@
  * Time: 5:05 PM
  */
 
-import edu.cmu.sphinx.frontend.FrontEnd;
+import edu.cmu.sphinx.frontend.*;
 import edu.cmu.sphinx.frontend.feature.DeltasFeatureExtractor;
 import edu.cmu.sphinx.frontend.feature.LiveCMN;
 import edu.cmu.sphinx.frontend.filter.Preemphasizer;
@@ -18,27 +18,26 @@ import edu.cmu.sphinx.frontend.window.RaisedCosineWindower;
 import java.net.URL;
 import java.util.ArrayList;
 
-
 public class CustomDecoder {
 
     private FrontEnd frontend;
     private String root = "/home/cody/IdeaProjects/nlu-speech-recognizer";
+    private String fp = "file:"+root+"resources/red.wav";
 
 
     /**
      * initialize the Sphinx4 frontend pipeline. This pipeline does the DSP for the audio inputs.
      */
-    protected void initFrontEnd() {
+    protected void initFrontEnd(String fp) {
         AudioFileDataSource audioDataSource = new AudioFileDataSource(3200, null);
-        String filepath = "file:"+root+"resources/red.wav";
 
         try {
-            URL audioURL = new URL(filepath);
+            URL audioURL = new URL(fp);
             audioDataSource.setAudioFile(audioURL, null);
         }
         catch (Exception ex)
         {
-            System.out.println("file not found: " + filepath);
+            System.out.println("file not found: " + fp);
         }
 
         Preemphasizer premphasizer = new Preemphasizer(
@@ -89,6 +88,30 @@ public class CustomDecoder {
 
         this.frontend = new FrontEnd(pipeline);
     }
+
+    private ArrayList<float[]> getFeatures() {
+        ArrayList<float[]> floatArrs = new ArrayList<float[]>();
+        Data data = frontend.getData();
+
+        while (! (data instanceof DataEndSignal)){
+            if (! (data instanceof DataStartSignal))
+            {
+                // cast data to FloatData and retrieve the float array, add it to floatArrs
+                floatArrs.add(((FloatData) data).getValues());
+            }
+        }
+        return floatArrs;
+    }
+
+    private void main(){
+        initFrontEnd(fp);
+        ArrayList<float[]> features = getFeatures();
+        for(int i=0;i<features.size();i++){
+           //System.out.println(features.);
+        }
+
+    }
+
 
 
 }
