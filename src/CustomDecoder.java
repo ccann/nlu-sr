67d3b074c71451f0c18b92868d6b3782ca665ddf
Cody Canning <cocanning11@gmail.com>
2013-03-21 14:41:35 -0400

    added binning procedure and pulled out word-specific functions into an Utterance class

--- src/CustomDecoder.java
+++ src/CustomDecoder.java
@@ -2,124 +2,17 @@
  * @author: ccann
  */
 
-import edu.cmu.sphinx.frontend.*;
-import edu.cmu.sphinx.frontend.feature.DeltasFeatureExtractor;
-import edu.cmu.sphinx.frontend.feature.LiveCMN;
-import edu.cmu.sphinx.frontend.filter.Preemphasizer;
-import edu.cmu.sphinx.frontend.frequencywarp.MelFrequencyFilterBank;
-import edu.cmu.sphinx.frontend.transform.DiscreteCosineTransform;
-import edu.cmu.sphinx.frontend.transform.DiscreteFourierTransform;
-import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
-import edu.cmu.sphinx.frontend.window.RaisedCosineWindower;
-
 import weka.classifiers.lazy.IBk;
 
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class CustomDecoder {
 
-    private static FrontEnd frontend;
     private static String root = "/home/cody/IdeaProjects/nlu-speech-recognizer/";
     private static String fp = "file:"+root+"resources/green.wav";
 
-    private static int NUM_CEPSTRA = 13;
-
-    /**
-     * initialize the Sphinx4 frontend pipeline. This pipeline does the DSP for the audio inputs.
-     */
-    protected static void initSphinxFrontEnd(String fp) {
-        AudioFileDataSource audioDataSource = new AudioFileDataSource(3200, null);
-
-        try {
-            URL audioURL = new URL(fp);
-            audioDataSource.setAudioFile(audioURL, null);
-        }
-        catch (Exception ex)
-        {
-            System.out.println("file not found: " + fp);
-        }
-
-        Preemphasizer preemphasizer = new Preemphasizer(
-                0.97 // preemphasisFactor
-        );
-
-        RaisedCosineWindower windower = new RaisedCosineWindower(
-                0.46, // double alpha
-                25.625f, // windowSizeInMs
-                10.0f // windowShiftInMs
-        );
-
-        DiscreteFourierTransform fft = new DiscreteFourierTransform(
-                -1, // numberFftPoints
-                false // invert
-        );
-
-        MelFrequencyFilterBank melFilterBank = new MelFrequencyFilterBank(
-                130.0, // minFreq,
-                6800.0, // maxFreq,
-                40 // numberFilters
-        );
-
-        DiscreteCosineTransform dct = new DiscreteCosineTransform(
-                40, // numberMelFilters,
-                13  // cepstrumSize
-        );
-
-        LiveCMN cmn = new LiveCMN(
-                12.0, // initialMean,
-                100,  // cmnWindow,
-                160   // cmnShiftWindow
-        );
-
-        DeltasFeatureExtractor featureExtraction = new DeltasFeatureExtractor(
-                3 // window
-        );
-
-        ArrayList pipeline = new ArrayList();
-        pipeline.add(audioDataSource);
-        pipeline.add(preemphasizer);
-        pipeline.add(windower);
-        pipeline.add(fft);
-        pipeline.add(melFilterBank);
-        pipeline.add(dct);
-        pipeline.add(cmn);
-        pipeline.add(featureExtraction);
-
-        frontend = new FrontEnd(pipeline);
-    }
-
-    /**
-     * get NUM_CEPSTRA cepstra from the sphinx4 frontend.
-     * @return cepstra for each window
-     */
-    private static ArrayList<float[]> getCepstra() {
-        ArrayList<float[]> floatArrs = new ArrayList<float[]>();
-        Data data = frontend.getData();
-
-
-        while (! (data instanceof DataEndSignal)){
-            if (! (data instanceof DataStartSignal))
-            {
-                // cast data to FloatData and retrieve the float array, add it to floatArrs
-                floatArrs.add(((FloatData) data).getValues());
-            }
-            data = frontend.getData();
-        }
-
-        ArrayList<float[]> cepstra = new ArrayList<float[]>(floatArrs.size());
-
-        for(int i=0;i<floatArrs.size();i++)
-        {
-            float[] temp = new float[NUM_CEPSTRA];
-            for (int j=0;j<NUM_CEPSTRA;j++){
-                temp[j] = floatArrs.get(i)[j];
-            }
-            cepstra.add(temp);
-        }
-        return cepstra;
-    }
-
+    private static int NUM_BINS = 5;
 
 //    private static ArrayList<Integer> compressCepstra(ArrayList<float[]> cepstra){
 //
@@ -138,10 +31,49 @@ public class CustomDecoder {
         }
     }
 
+    /**
+     * dumps the cepstra from the cepstra vectors into NUM_BINS bins
+     * @param utt  the utterance from which we're getting the cepstra
+     * @return a list containing the the cepstra bins
+     */
+    private ArrayList<ArrayList<Float>> getBinnedCepstra (Utterance utt){
+
+        ArrayList<ArrayList<Float>> bins = new ArrayList<ArrayList<Float>>(NUM_BINS);
+        LinkedList<float[]> cepstraVectors = utt.getCepstra();
+
+        // the number of cepstra vectors to be dumped into each bin
+        int binSize = utt.getNumWindows() / NUM_BINS;
+
+        for (int i = 0; i < NUM_BINS; i++){
+
+            ArrayList<Float> bin = new ArrayList<Float>(binSize);
+            for(int j =0; j < binSize ; j++){
+
+                // pop first cepstra vector in cepstraVectors
+                float[] vec = cepstraVectors.remove();
+                for (float cepstrum : vec){
+                    // add the floats from the vector to the bin
+                    bin.add(cepstrum);
+                }
+            }
+            bins.add(bin);
+        }
+
+        // in the case that there are extra cepstra vectors (remainder of binSize), add the cepstra to the last bin.
+        if (! cepstraVectors.isEmpty()){
+            for (float[] cepstra : cepstraVectors){
+                for (int i=0;i<cepstra.length;i++){
+                    bins.get(NUM_BINS-1).add(cepstra[i]);
+                }
+            }
+        }
+        return bins;
+    }
+
     public static void main(String args[]) {
-        initSphinxFrontEnd(fp);
-        ArrayList<float[]> features = getCepstra();
-        printFeatures(features);
+        //initSphinxFrontEnd(fp);
+        //ArrayList<float[]> features = getCepstra();
+        //printFeatures(features);
     }
 
 
---
+++ src/Utterance.java
@@ -0,0 +1,145 @@
+import edu.cmu.sphinx.frontend.*;
+import edu.cmu.sphinx.frontend.feature.DeltasFeatureExtractor;
+import edu.cmu.sphinx.frontend.feature.LiveCMN;
+import edu.cmu.sphinx.frontend.filter.Preemphasizer;
+import edu.cmu.sphinx.frontend.frequencywarp.MelFrequencyFilterBank;
+import edu.cmu.sphinx.frontend.transform.DiscreteCosineTransform;
+import edu.cmu.sphinx.frontend.transform.DiscreteFourierTransform;
+import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
+import edu.cmu.sphinx.frontend.window.RaisedCosineWindower;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cody
+ * Date: 3/21/13
+ * Time: 1:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Utterance {
+
+    private int numWindows;
+    private String filepath;
+    private FrontEnd frontend;
+    private static int NUM_CEPSTRA = 13;
+
+
+    public void setNumWindows(int n){
+        numWindows = n;
+    }
+
+    public int getNumWindows(){
+        return numWindows;
+    }
+
+    public String getFilePath(){
+        return filepath;
+    }
+
+    /**
+     * get the cepstra arrays from the sphinx4 frontend.
+     * @return cepstra for each window
+     */
+    public LinkedList<float[]> getCepstra() {
+        initFrontend(filepath);
+
+        ArrayList<float[]> floatArrs = new ArrayList<float[]>();
+        Data data = frontend.getData();
+
+        while (! (data instanceof DataEndSignal)){
+            if (! (data instanceof DataStartSignal))
+            {
+                // cast data to FloatData and retrieve the float array, add it to floatArrs
+                floatArrs.add(((FloatData) data).getValues());
+            }
+            data = frontend.getData();
+        }
+
+        this.numWindows = floatArrs.size();
+
+        LinkedList<float[]> cepstra = new LinkedList<float[]>();
+
+        for(int i=0;i<floatArrs.size();i++)
+        {
+            float[] temp = new float[NUM_CEPSTRA];
+            for (int j=0;j<NUM_CEPSTRA;j++){
+                temp[j] = floatArrs.get(i)[j];
+            }
+            cepstra.add(temp);
+        }
+        return cepstra;
+    }
+
+    /**
+     * initialize the Sphinx4 frontend pipeline. This pipeline does the DSP for the audio inputs.
+     */
+    protected void initFrontend(String fp) {
+        AudioFileDataSource audioDataSource = new AudioFileDataSource(3200, null);
+
+        try {
+            URL audioURL = new URL(fp);
+            audioDataSource.setAudioFile(audioURL, null);
+        }
+        catch (Exception ex)
+        {
+            System.out.println("file not found: " + fp);
+        }
+
+        Preemphasizer preemphasizer = new Preemphasizer(
+                0.97 // preemphasisFactor
+        );
+
+        RaisedCosineWindower windower = new RaisedCosineWindower(
+                0.46, // double alpha
+                25.625f, // windowSizeInMs
+                10.0f // windowShiftInMs
+        );
+
+        DiscreteFourierTransform fft = new DiscreteFourierTransform(
+                -1, // numberFftPoints
+                false // invert
+        );
+
+        MelFrequencyFilterBank melFilterBank = new MelFrequencyFilterBank(
+                130.0, // minFreq,
+                6800.0, // maxFreq,
+                40 // numberFilters
+        );
+
+        DiscreteCosineTransform dct = new DiscreteCosineTransform(
+                40, // numberMelFilters,
+                13  // cepstrumSize
+        );
+
+        LiveCMN cmn = new LiveCMN(
+                12.0, // initialMean,
+                100,  // cmnWindow,
+                160   // cmnShiftWindow
+        );
+
+        DeltasFeatureExtractor featureExtraction = new DeltasFeatureExtractor(
+                3 // window
+        );
+
+        ArrayList pipeline = new ArrayList();
+        pipeline.add(audioDataSource);
+        pipeline.add(preemphasizer);
+        pipeline.add(windower);
+        pipeline.add(fft);
+        pipeline.add(melFilterBank);
+        pipeline.add(dct);
+        pipeline.add(cmn);
+        pipeline.add(featureExtraction);
+
+        frontend = new FrontEnd(pipeline);
+    }
+
+    public Utterance(String f){
+        this.filepath = f;
+        initFrontend(this.filepath);
+    }
+
+}
